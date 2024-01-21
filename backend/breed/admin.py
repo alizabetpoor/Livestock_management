3e26437b5b89bdf9583d989b843875eb179b371b
ali.zabetpour <ali_0_zabetpoor@ymail.com>
2024-01-21 10:22:38 +0330

    modify breed admin

--- backend/breed/admin.py
+++ backend/breed/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Breed
 
-# Register your models here.
 
-admin.site.register(Breed)
+# Register your models here.
+class Breed_Admin(admin.ModelAdmin):
+    list_display = ("name", "origin", "average_weight", "life_expectancy", "color")
+
+
+admin.site.register(Breed, Breed_Admin)
