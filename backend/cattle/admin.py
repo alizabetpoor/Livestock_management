84e9b361cfb30b6899f0be33bda131bbd4715c19
ali.zabetpour <ali_0_zabetpoor@ymail.com>
2024-01-21 10:21:14 +0330

    modify cattle admin

--- backend/cattle/admin.py
+++ backend/cattle/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Cattle, BreedingRecord
 
-# Register your models here.
 
-admin.site.register(Cattle)
-admin.site.register(BreedingRecord)
+# Register your models here.
+class Cattle_Admin(admin.ModelAdmin):
+    list_display = (
+        "ear_tag_number",
+        "breed",
+        "gender",
+        "date_of_birth",
+        "weight",
+        "BreedingRecord",
+    )
+
+
+class BreedingRecord_Admin(admin.ModelAdmin):
+    list_display = ("bull", "cow", "breeding_date", "expected_calving_date")
+
+
+admin.site.register(Cattle, Cattle_Admin)
+admin.site.register(BreedingRecord, BreedingRecord_Admin)
