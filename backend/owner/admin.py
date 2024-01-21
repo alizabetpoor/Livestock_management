039f2f64b160ab38068f51238cd6eb1181f1545c
ali.zabetpour <ali_0_zabetpoor@ymail.com>
2024-01-21 10:23:36 +0330

    modify owner admin

--- backend/owner/admin.py
+++ backend/owner/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Owner
 
-# Register your models here.
 
-admin.site.register(Owner)
+# Register your models here.
+class Owner_Admin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "phone_number", "email", "created")
+
+
+admin.site.register(Owner, Owner_Admin)
